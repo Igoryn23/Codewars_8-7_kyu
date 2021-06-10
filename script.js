255493ddaa9e0c2d698ed8ea20fcbd8be21151d0
Igoryn23 <igoryn9702@gmail.com>
2021-06-10 22:14:27 +0300

    add: All Star Code Challenge #22( 7 kyu )

--- script.js
+++ script.js
@@ -11,4 +11,75 @@ function mergeArrays(a, b) {
     console.log(arr);
 }
 
-mergeArrays([1, 2, 3, 4, 5, 6, 7], ['a', 'b', 'c']);
+// mergeArrays([1, 2, 3, 4, 5, 6, 7], ['a', 'b', 'c']);
+
+//  explode 
+
+function explode(x) {
+    let arr = [];
+    let x1 = x[0];
+    let x2 = x[1];
+    /// проверка для void
+    if (typeof x1 == 'string' && typeof x2 == 'string') {
+        return 'Void!';
+    }
+    ///  проверка для 2х int
+    if (typeof x1 === 'number' && typeof x2 === 'number') {
+        let c = x1 + x2;
+        for (let i = 0; i < c; i++) {
+            arr.push(x);
+        }
+        return arr;
+    }
+    // x1 = string
+    if (typeof x1 == 'string') {
+        for (let i = 0; i < x2; i++) {
+            arr.push(x);
+        }
+        return arr;
+    }
+    // x2 = string
+    if (typeof x2 == 'string') {
+        for (let i = 0; i < x1; i++) {
+            arr.push(x);
+        }
+        return arr;
+    }
+
+}
+
+// explode(['t', 3]);
+
+function noBoringZeros(n) {
+
+
+
+    let arr = [];
+    let a = 0;
+    let temp = n.toString().split('');
+    console.log(temp);
+    for (let i = temp.length - 1; i >= 0; i--) {
+        if (temp[i] !== '0') {
+            a = i;
+            break;
+        }
+    }
+    console.log(a);
+    for (let k = 0; k <= a; k++) {
+        arr.push(temp[k]);
+    }
+    console.log(arr);
+
+}
+
+// noBoringZeros(-145000);
+
+// All Star Code Challenge #22
+function toTime(seconds) {
+    let hours = Math.floor(seconds / 3600);
+    let min = Math.floor((seconds - hours * 3600) / 60);
+    console.log(hours, min);
+    return `${hours} hour(s) and ${min} minute(s)`;
+
+}
+toTime(3663);
