03123523c882f8b4b6ccc6e167d9662d5647ace3
Igoryn23 <igoryn9702@gmail.com>
2021-06-21 15:29:32 +0300

    add: which triangle is that

--- script.js
+++ script.js
@@ -167,3 +167,19 @@ function count(array) {
 }
 
 count(['a', 'a', 'b', 'b', 'b']);
+
+// Which triangle is that?
+// Build a function that will take the length of each side of a triangle and return if it's either an Equilateral, an Isosceles, a Scalene or an invalid triangle.
+
+var typeOfTriangle = function(a, b, c) {
+    if (!(a + b > c && a + c > b && b + c > a)) {
+        return 'Not a valid triangle';
+    }
+    if (a === b && a === c && b === c) {
+        return 'Equilateral';
+    } else if (a === b || a === c || b === c) {
+        return 'Isosceles';
+    } else {
+        return 'Scalene';
+    }
+}
